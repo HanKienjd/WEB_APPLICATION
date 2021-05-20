6f17933eb37305bd59ba388a793dcb713591ceec
Vu Trung Kien <kienvutrung20@gmail.com>
2021-05-20 16:42:45 +0700

    Create tables

---
+++ database/migrations/20200817112822_create_user_table.js
@@ -0,0 +1,25 @@
+exports.up = async (knex) => {
+  await knex.schema.createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('email', 127).collate('latin1_general_ci').notNullable();
+      table.string('full_name', 50).collate('utf8_general_ci').notNullable();
+      table.string('username', 20).collate('latin1_general_ci').notNullable();
+      table.string('password', 20).collate('latin1_general_ci').notNullable();
+      table.string('address', 127).collate('utf8_general_ci').notNullable();
+      table.string('gender', 3).collate('utf8_general_ci').notNullable();
+      table.date('date_of_birth');
+      table.string('image', 120).collate('latin1_general_ci').notNullable();
+      table.string('role', 10).collate('latin1_general_ci').notNullable();
+
+      table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+      table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+      table.unique('email');
+      table.unique('username');
+      table.unique('image');
+  });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTable('users');
+};	
---
+++ database/migrations/20210520163311_create_order_detail_table.js
@@ -0,0 +1,20 @@
+exports.up = async (knex) => {
+    await knex.schema.createTable('order_detail', (table) => {
+        table.increments('id').primary();
+        table.integer('price').unsigned().notNullable();
+        table.integer('quantity').unsigned().notNullable();
+
+        table.integer('order_id').unsigned().references('orders.id').notNullable(); 
+        table.integer('product_id').unsigned().references('products.id').notNullable();
+
+        table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+        table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+        table.index('order_id');
+        table.index('product_id');
+    });
+};
+
+exports.down = function(knex) {
+    await knex.schema.dropTable('order_detail');
+};
