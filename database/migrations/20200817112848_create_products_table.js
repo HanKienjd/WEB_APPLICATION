6b31e326e7f9e3babe5cf2b148f82edd537d192c
hoan.nguyenduy <hoan.nguyenduy@amela.vn>
2021-05-20 23:00:06 +0700

    Fix: migrate

--- database/migrations/20200817112848_create_products_table.js
+++ database/migrations/20200817112848_create_products_table.js
@@ -1,27 +1,27 @@
 exports.up = async (knex) => {
   await knex.schema.createTable('products', (table) => {
-      table.increments('id').primary();
-      table.string('name', 50).collate('utf8_general_ci').notNullable();
-      table.string('status', 50).collate('utf8_general_ci').notNullable();
-      table.integer('buying_price').unsigned().notNullable();
-      table.integer('selling_price').unsigned().notNullable();
-      table.integer('quantity').unsigned().notNullable();
-      table.string('description', 120).collate('utf8_general_ci').notNullable();
-      table.string('image', 120).collate('latin1_general_ci').notNullable();
+    table.increments('id').primary();
+    table.string('name', 50).collate('utf8_general_ci').notNullable();
+    table.string('status', 50).collate('utf8_general_ci').notNullable();
+    table.integer('buying_price').unsigned().notNullable();
+    table.integer('selling_price').unsigned().notNullable();
+    table.integer('quantity').unsigned().notNullable();
+    table.string('description', 120).collate('utf8_general_ci').notNullable();
+    table.string('image', 120).collate('latin1_general_ci').notNullable();
 
-      table.integer('category_id').unsigned().references('categories.id').notNullable();
+    table.integer('category_id').unsigned().references('categories.id').notNullable();
 
-      table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
-      table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+    table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+    table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
-      table.unique('name');
-      table.unique('description');
-      table.unique('image');
+    table.unique('name');
+    table.unique('description');
+    table.unique('image');
 
-      table.index('category_id');
+    table.index('category_id');
   });
 };
 
-exports.down = function(knex) {
+exports.down = async (knex) => {
   await knex.schema.dropTable('products');
 };
