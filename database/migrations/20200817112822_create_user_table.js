628eb9d6782064691539487ab103600022e05cea
Vu Trung Kien <kienvutrung20@gmail.com>
2021-05-20 20:22:59 +0700

    Update user table

--- database/migrations/20200817112822_create_user_table.js
+++ database/migrations/20200817112822_create_user_table.js
@@ -9,14 +9,13 @@ exports.up = async (knex) => {
       table.string('gender', 3).collate('utf8_general_ci').notNullable();
       table.date('date_of_birth');
       table.string('image', 120).collate('latin1_general_ci').notNullable();
-      table.string('role', 10).collate('latin1_general_ci').notNullable();
+      table.integer('role', 1).unsigned().notNullable();
 
       table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
       table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
       table.unique('email');
       table.unique('username');
-      table.unique('image');
   });
 };
 
