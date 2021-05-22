b5fa688c96be057c6477c0d233fd69545e8ba968
hoan.nguyenduy <hoan.nguyenduy@amela.vn>
2021-05-22 17:25:00 +0700

    Add: authencation

--- app/routes/product.js
+++ app/routes/product.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const { product: productController } = require('../http/controllers');
 const upload = require('../http/middlewares/uploadFile');
+const { auth } = require('../http/middlewares');
 
 const router = express.Router();
 
-router.post('/products', upload.single('productImg'), productController.create);
+router.post('/products', auth, upload.single('productImg'), productController.create);
 router.get('/products', productController.getList);
 router.get('/products/:productId', productController.getDetail);
-router.put('/products/:productId', upload.single('productImg'), productController.update);
-router.delete('/products/:productId', productController.remove);
+router.put('/products/:productId', auth, upload.single('productImg'), productController.update);
+router.delete('/products/:productId', auth, productController.remove);
 
 module.exports = router;
