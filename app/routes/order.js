060fba524e639e36b97729fa547c300b4285db6c
hoannguyen <hoan.nguyenduy@amela.vn>
2021-07-02 14:48:05 +0700

    Add: check permission

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
 const { order: orderController } = require('../http/controllers');
-const checkPermission = require('../http/middlewares/permission');
 
 const router = express.Router();
 
-router.post('/orders', checkPermission, orderController.create);
+router.post('/orders', orderController.create);
 router.get('/orders', orderController.getList);
-router.get('/orders/:orderId', checkPermission, orderController.getOrderDetail);
+router.get('/orders/:orderId', orderController.getOrderDetail);
 
 module.exports = router;
